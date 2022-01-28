f4b8d86620e76ac0e473918371c5a042f99ede86
Lucas Pacheco <lucaspdroz@gmail.com>
2022-01-28 11:36:49 -0300

    chore: add testing render Person (toHaveText, toHaveAttribute, toBeInDocument, getByRole)

---
+++ src/Person.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Person } from './Person';
+
+test('renders a name', () => {
+    render(<Person name="Lucas" />);
+    const componentName = screen.getByText(/Name is Lucas/i);
+    expect(componentName).toBeInTheDocument();
+});
+
+test('renders a name with a refined test', () => {
+    render(<Person name="Lucas" />);
+    const divElement = screen.getByRole("contentinfo");
+    expect(divElement).toHaveTextContent("Name is Lucas");
+    expect(divElement).toHaveAttribute("role", "contentinfo");
+});
